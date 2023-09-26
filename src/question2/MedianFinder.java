74287142c5adc50e809edcc534d12aefbf629e74
Kishan Kumar Zalavadia <kishankumarz1048@gmail.com>
2023-09-25 22:57:49 -0500

    Written expanation for question-2

--- src/question2/MedianFinder.java
+++ src/question2/MedianFinder.java
@@ -2,7 +2,9 @@ package question2;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class MedianFinder {
 
@@ -44,36 +46,22 @@ public class MedianFinder {
         return result;
     }
 
-
-
-    public double findMedian(List<Integer> sortedLists) {
-
-        int totalSize = sortedLists.size();
-        int middle = totalSize/2;
-        if(totalSize % 2 == 0){
-            // If even number of elements, medium the average of the two middle elements
-            return (sortedLists.get(middle - 1)+sortedLists.get(middle))/2.0;
+    // Function to find the median of a sorted list.
+    public double findMedian(List<Integer> sortedList) {
+        int totalSize = sortedList.size();
+        int middle = totalSize / 2;
+        if (totalSize % 2 == 0) {
+            // If even number of elements, the median is the average of the two middle elements.
+            return (sortedList.get(middle - 1) + sortedList.get(middle)) / 2.0;
+        } else {
+            // If an odd number of elements, the median is the middle element.
+            return sortedList.get(middle);
         }
-        else{
-            // If odd number of elements, medium is middle element
-            return sortedLists.get(middle);
-        }
-
-
-//        if (totalSize % 2 == 0) {
-//            // If even number of elements, return the average of the two middle elements
-//            return (maxHeap.peek() + minHeap.peek()) / 2.0;
-//        } else {
-//            // If odd number of elements, return the middle element from the maxHeap
-//            return maxHeap.peek();
-//        }
     }
 
-
-
     public static void main(String[] args) throws FileNotFoundException {
-
-        Scanner fileScanner = new Scanner(new File("/Users/kishankumarz/Library/CloudStorage/GoogleDrive-kishankumarz3131@gmail.com/My Drive/Important/[02] USA/[02] UNT/[02] Classes/[01] Analysis_of Computer_Algoritms/[02] ACA_Assisgnment/[01] HomeWork/ACA_HomeWork-1/src/question1/input")); // Open the file for reading
+        String inputFilePath = "/Users/kishankumarz/Library/CloudStorage/GoogleDrive-kishankumarz3131@gmail.com/My Drive/Important/[02] USA/[02] UNT/[02] Classes/[01] Analysis_of Computer_Algoritms/[02] ACA_Assisgnment/[01] HomeWork/ACA_HomeWork-1/src/question2/input";
+        Scanner fileScanner = new Scanner(new File(inputFilePath));
 
         List<List<Integer>> inputSortedList = new ArrayList<>();
 
@@ -93,18 +81,15 @@ public class MedianFinder {
             inputSortedList.add(innerList);
         }
         System.out.println("_____________________________________________________________________________________");
-        System.out.println("Input: "+inputSortedList);
+        System.out.println("Input: " + inputSortedList);
 
         List<Integer> mergedList = mergeNSortedArrayLists(inputSortedList);
-        System.out.println("Sorted Array: "+mergedList);
+        System.out.println("Sorted Array: " + mergedList);
 
         MedianFinder finder = new MedianFinder();
         double median = finder.findMedian(mergedList);
         System.out.println("Median: " + median);
         System.out.println("_____________________________________________________________________________________");
         fileScanner.close();
-
     }
 }
-
-
